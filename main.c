f38faffcfb11ffe92f5cf783543eccadaca793de
Lilian SOLER <soler.lilian64@gmail.com>
2021-07-02 11:47:23 +0200

    Première version du jeu (moteur de jeu seulement)

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    printf("Hello, World!\n");
+    srand(time(NULL));
+    const int MIN = 1, MAX = 100;
+    int nombreMystere = (rand() % (MAX - MIN + 1)) + MIN,nombrePropose = 0;
+
+    printf("Bienvenu sur le jeu du plus ou moins.\n"
+               "Vous devez devinez un nombre compris entre %d et %d\n\n",MIN,MAX);
+
+    while(nombreMystere != nombrePropose)
+    {
+        printf("Quel est le nombre ?\n");
+        scanf("%d",&nombrePropose);
+        if(nombrePropose < nombreMystere)
+        {
+            printf("C'est plus !\n\n");
+        }
+        else if(nombrePropose > nombreMystere)
+        {
+            printf("C'est moins !\n\n");
+        }
+    }
+    printf("Bravo vous avez gagnez, le nombre était bien %d \n\n", nombreMystere);
     return 0;
 }
