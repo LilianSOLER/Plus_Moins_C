53e57fb409fbaa5c7fa94d72dd597bd5ffa81040
Lilian SOLER <soler.lilian64@gmail.com>
2021-07-02 12:00:55 +0200

    MAJ 1.2 - Ajout de la fonctionnalité Rejouer

--- main.c
+++ main.c
@@ -5,25 +5,27 @@
 int main() {
     srand(time(NULL));
     const int MIN = 1, MAX = 100;
-    int nombreMystere = (rand() % (MAX - MIN + 1)) + MIN,nombrePropose = 0, essais = 0;
-
-    printf("Bienvenu sur le jeu du plus ou moins.\n"
-               "Vous devez devinez un nombre compris entre %d et %d\n\n",MIN,MAX);
-
-    while(nombreMystere != nombrePropose)
+    int continuerPartie = 1;
+    while(continuerPartie == 1)
     {
-        printf("Quel est le nombre ?\n");
-        scanf("%d",&nombrePropose);
-        if(nombrePropose < nombreMystere)
-        {
-            printf("C'est plus !\n\n");
+        int nombreMystere = (rand() % (MAX - MIN + 1)) + MIN,nombrePropose = 0, essais = 0;
+
+        printf("Bienvenu sur le jeu du plus ou moins.\n"
+               "Vous devez devinez un nombre compris entre %d et %d\n\n", MIN, MAX);
+
+        while (nombreMystere != nombrePropose) {
+            printf("Quel est le nombre ?\n");
+            scanf("%d", &nombrePropose);
+            if (nombrePropose < nombreMystere) {
+                printf("C'est plus !\n\n");
+            } else if (nombrePropose > nombreMystere) {
+                printf("C'est moins !\n\n");
+            }
+            essais++;
         }
-        else if(nombrePropose > nombreMystere)
-        {
-            printf("C'est moins !\n\n");
-        }
-        essais++;
+        printf("Bravo vous avez gagnez en %d coup, le nombre était bien %d \n\n", essais, nombreMystere);
+        printf("Pour continuer tapez 1, et appuyer sur n'importe quel autre touche pour arreter\n\n");
+        scanf("%d",&continuerPartie);
     }
-    printf("Bravo vous avez gagnez en %d coup, le nombre était bien %d \n\n", essais, nombreMystere);
     return 0;
 }
